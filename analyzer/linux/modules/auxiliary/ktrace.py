a87798af6a107d5139ac880d091ecb00284d1c15
helabai <helabai@QQ.com>
2016-12-02 09:12:07 +0800

    ktrace kernel module ok

--- analyzer/linux/modules/auxiliary/ktrace.py
+++ analyzer/linux/modules/auxiliary/ktrace.py
@@ -10,39 +10,40 @@ import platform
 
 from lib.common.abstracts import Auxiliary
 from lib.common.results import NetlogFile
+from lib.common.results import upload_to_host 
 from lib.core.config import Config
 
 log = logging.getLogger(__name__)
-klog = "/var/log/kern.log"
 
 class KTRACE(Auxiliary):
     """ktrace catch all syscall."""
-    priority = -5 # low prio to wrap tightly around the analysis
+    priority = -5 
 
     def __init__(self):
         self.config = Config(cfg="analysis.conf")
         self.fallback_strace = False
-
     def start(self):
-        pid = os.getpid()
-        log.info(">>>>>>>>>> ktrace start: %d <<<<<<<<<<",pid)
-        f = open(klog, "rw+")
-        f.truncate()
-        f.close()
+        log.info("ktrace start() %d" % os.getpid())
+        os.system("/bin/dmesg -c &>/dev/null")
+        root_path = os.getcwd()
+        bin_path = os.path.join(root_path,"bin")
+        module_path = os.path.join(bin_path, "ktrace.ko")
+        if os.path.exists(module_path):
+            os.system("/sbin/insmod %s analyzer=%d" % (module_path,os.getpid()))
+            log.info("insmod command execute")
+        else:
+            log.info("kernel module not there")
+        log.info("analyzer path:%s" % module_path)
+        log.info("ktrace start() return versin 0.01")
         return True
 
     def get_pids(self):
         return []
 
     def stop(self):
-        if os.path.exists(klog):
-            # now upload the logfile
-            log.info("Guest send kernel log -> Host")
-            nf = NetlogFile("logs/kern.log")
-            fd = open(klog, "rb")
-            for chunk in fd:
-                nf.sock.sendall(chunk)
-            fd.close()
-            nf.close()
-        log.info(">>>>>>>>>> ktrace stop  %d <<<<<<<<<<",os.getpid())
-
+        log.info("ktrace stop() %d" % os.getpid())
+        os.system("/sbin/rmmod ktrace")
+        os.system("/bin/dmesg > /var/log/ktrace.log")
+        upload_to_host("/var/log/ktrace.log","logs/ktrace.log")
+        upload_to_host("/proc/version","files/version")
+        upload_to_host("/proc/modules","files/modules")
